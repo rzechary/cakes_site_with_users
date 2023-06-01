e1cb18eca4aa85afa7effdbe87936b64745d4b54
rzechary <102223789+rzechary@users.noreply.github.com>
2023-06-01 14:16:14 +0300

    finish

--- routes/config_routes.js
+++ routes/config_routes.js
@@ -1,11 +1,13 @@
 const indexR= require("./index");
 const usersR= require("./users");
 const cakesR= require("./cakes");
+const countryR= require("./countries");
 
 
 exports.routesInit =(app)=>{
     app.use("/",indexR)
     app.use("/users",usersR)
-   app.use("/cakes",cakesR)
+    app.use("/cakes",cakesR)
+    app.use("/countries",countryR)
   
 }
